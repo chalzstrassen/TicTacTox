c4c4256be0b40a461cdd133435e179f9eab1a72b
chalzstrassen <yuchao.137chen@gmail.com>
2015-04-24 20:27:40 -0400

    Add render function and begin work on win conditions.

--- TTT/board.js
+++ TTT/board.js
@@ -7,5 +7,17 @@ var Board = function () {
 };
 
 Board.prototype.render = function () {
-  
+  this.grid.forEach(function (el) {
+  	console.log(JSON.stringify(el));
+  });
+};
+
+Board.prototype.isWon = function (token) {
+  //check rows
+  this.grid.forEach(function (row, idx, arr){
+  	if (row.every(function (el) { return el === token;})) {
+  		return true;
+  	} else if ()
+
+  });
 };
